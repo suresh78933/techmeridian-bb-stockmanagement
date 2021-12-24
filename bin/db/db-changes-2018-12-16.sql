b6c6f332bf107907aaedc8d7b18321f38a8e4c18
Suresh BGV <bgvsuresh@google.com>
2021-12-24 17:06:15 +0530

    Removed duplicate files

--- bin/db/db-changes-2018-12-16.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `stock_management`.`item_master` 
-ADD COLUMN `inventory` VARCHAR(16) NULL  AFTER `company_id`,
-ADD COLUMN `part_no` VARCHAR(32) NULL AFTER `inventory`;
-
-ALTER TABLE `stock_management`.`purchase_order_lines` 
-ADD COLUMN `part_no` VARCHAR(32) NULL AFTER `change_key`;
